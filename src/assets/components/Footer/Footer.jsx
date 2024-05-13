810defe2e8a23434de7dbdd5239c8451f714d375
Andres Barroso <abarroso@nectia.com>
2024-05-13 04:03:05 -0400

    New build, location component added

--- src/assets/components/Footer/Footer.jsx
+++ src/assets/components/Footer/Footer.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Input } from '@chakra-ui/react'
 import './Footer.css'
 
 export const Footer = () => {
   return (
-    <div className='footer'>Footer</div>
+    <div className='footer'>
+      <p>Suscríbete a la newsletter para estar al día en últimas tendencias, también te enviaremos promociones de nuestra peluquería.</p>
+      <Input placeholder='large size' size='lg' />
+      <Input placeholder='large size' size='lg' />
+    </div>
   )
 }
--- src/assets/components/Location/Location.jsx
+++ src/assets/components/Location/Location.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
-import './Location.css'
+import React from "react";
+import "./Location.css";
 
 export const Location = () => {
   return (
-    <div className='location'>Location</div>
-  )
-}
+    <div className="location">
+      <h1 className="location-title">Encuentra aquí nuestro Salón:</h1>
+      <iframe
+        className="map"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3327.158684878466!2d-70.65439239297474!3d-33.49724832996495!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662db73903203dd%3A0x63da20ec0826901f!2sValentinna%20Sal%C3%B3n!5e0!3m2!1ses-419!2scl!4v1715584621484!5m2!1ses-419!2scl"
+        width="600"
+        height="450"
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
+  );
+};
